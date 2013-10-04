2f98a114da89d7945f2b3b007fed2142289dcf0c
sqljiang <tjjiang0916@gmail.com>
2013-10-04 19:44:37 +0800

    version1.0.0

--- src/com/neusoft/encrypt/singet/Encryption.java
+++ src/com/neusoft/encrypt/singet/Encryption.java
@@ -2,6 +2,7 @@ package com.neusoft.encrypt.singet;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 public interface Encryption<T extends Enum<? extends Algorithm>> {
@@ -10,17 +11,21 @@ public interface Encryption<T extends Enum<? extends Algorithm>> {
 	 * @param plain
 	 * @param algorithm
 	 * @return
+	 * @throws IllegalStateException 
+	 * @throws InvalidKeyException 
 	 * @throws NotSuchMDAlgorithm
 	 */
-	public byte[] encrypt(byte[] plain,T algorithm)throws NoSuchAlgorithmException;
+	public byte[] encrypt(byte[] plain,T algorithm)throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException;
 	/**
 	 * encrypt string data by all kinds of MD algorithm
 	 * @param plaint
 	 * @param algorithm
 	 * @return
+	 * @throws IllegalStateException 
+	 * @throws InvalidKeyException 
 	 * @throws NotSuchMDAlgorithm
 	 */
-	public byte[] encrypt(String plain,T algorithm)throws NoSuchAlgorithmException;
+	public byte[] encrypt(String plain,T algorithm)throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException;
 	/**
 	 * encrypt input stream by all kinds of MD algorithm
 	 * @param input
