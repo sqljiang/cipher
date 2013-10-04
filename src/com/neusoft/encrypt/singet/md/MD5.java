84c012e3c653555623eadb7e71b94d1f89d6123e
sqljiang <tjjiang0916@gmail.com>
2013-10-04 11:55:53 +0800

    version1.0.0

--- src/com/neusoft/encrypt/singet/md/MD5.java
+++ src/com/neusoft/encrypt/singet/md/MD5.java
@@ -5,15 +5,15 @@ import java.security.NoSuchAlgorithmException;
 
 import org.apache.commons.lang.Validate;
 
-public class MD2 {
+public class MD5 {
 
-	private final static String ALGORITHM = "MD2";
+	private final static String ALGORITHM = "MD5" ;
 	
 	public byte[] encrypt(byte[] plaint) throws NoSuchAlgorithmException{
 		Validate.notNull(plaint, "the argument should not be null");
 		MessageDigest msg = MessageDigest.getInstance(ALGORITHM);
 		return msg.digest(plaint);
-	}
+ 	}
 	
 	public byte[] encrypt(String plaint) throws NoSuchAlgorithmException{
 		Validate.notNull(plaint, "the argument should not be null");
